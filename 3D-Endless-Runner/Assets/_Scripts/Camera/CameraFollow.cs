75de1c2b87da30801f63c64656be0a42df53ffe3
Jose <joey_manuel@outlook.com>
2020-12-26 11:56:08 -0500

    4.PlayerMovement
    finished with the Player script.

---
+++ 3D-Endless-Runner/Assets/_Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    #region Variables
+
+    private Transform playerTarget;
+
+    public float offsetZ = -15f;
+    public float offsetX = -5f;
+    public float constantY = 5f;
+    public float cameraLerpTime = 0.05f;
+    #endregion
+
+
+    private void Awake()
+    {
+        playerTarget = GameObject.FindGameObjectWithTag(Tags.PLAYER_TAG).transform;
+    }
+
+    private void FixedUpdate()
+    {
+        if(playerTarget)
+        {
+            Vector3 targetPosition = new Vector3(playerTarget.position.x + offsetX,
+                constantY, playerTarget.position.z + offsetZ);
+
+            transform.position = Vector3.Lerp(transform.position, targetPosition, cameraLerpTime);
+        }
+    }
+
+
+
+
+
+
+} // CameraFollow scripts
---
+++ 3D-Endless-Runner/Assets/_Scripts/Player/Player.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    #region Variables
+
+    [SerializeField]
+    private float movementSpeed = 5f;
+    [SerializeField]
+    private float jumpPower = 10f;
+    [SerializeField]
+    private float secondJumpPower = 10f;
+
+    [SerializeField]
+    private float radius = 0.3f;
+
+    public Transform groundCheckPosition;   // checks if the player is standing on the ground.
+    public LayerMask layerGround;
+    
+
+    private Rigidbody myBody;
+    private bool isGrounded;
+    private bool playerJumped = false;
+    private bool canDoubleJump = false;
+
+    // private PlayerAnimation playerAnim;
+
+    public GameObject smokePosition;
+
+    [SerializeField]
+    private bool gameStarted;    // check in the inspector;
+
+    //private BGScroller bgScroller;
+
+    // private PlayerHealthDamageShoot playerShoot;
+
+    private Button jumpButton;
+    #endregion
+
+    private void Awake()
+    {
+        myBody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerMove();
+        PlayerGrounded();
+        PlayerJump();
+    }
+    
+    void PlayerMove()
+    {
+        myBody.velocity = new Vector3(movementSpeed, myBody.velocity.y, 0f);
+    }
+
+    void PlayerGrounded()
+    {
+        isGrounded = Physics.OverlapSphere(groundCheckPosition.position, radius, layerGround).Length > 0;
+    }
+
+    void PlayerJump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && !isGrounded && canDoubleJump)
+        {
+            canDoubleJump = false;
+            myBody.AddForce(new Vector3(0, secondJumpPower, 0));
+        }
+        else if(Input.GetKeyUp(KeyCode.Space) && isGrounded)
+        {
+            //playerAnim.DidJump();
+            myBody.AddForce(new Vector3(0, jumpPower, 0));
+            playerJumped = true;
+            canDoubleJump = true;
+        }
+    }
+
+    public void Jump()
+    {
+        if(isGrounded)
+        {       
+            //playerAnim.Didjump();
+            myBody.AddForce(new Vector3(0, jumpPower, 0));
+            playerJumped = true;
+            canDoubleJump = true;
+
+        }
+
+        if(!isGrounded && canDoubleJump)
+        {
+            canDoubleJump = false;
+            myBody.AddForce(new Vector3(secondJumpPower, 0));
+        }
+
+    }
+
+    IEnumerator StartGame()
+    {
+        yield return new WaitForSeconds(2f);
+
+        gameStarted = true;
+        //bgScroller.canScrool = true;
+        //playerShoot.canShoot = true;
+        //GameplayController.instance.canCountScore = true;
+        smokePosition.SetActive(true);
+        //playerAnim.PlayerRun();
+    }
+
+    private void OnCollisionEnter(Collision target)
+    {
+        if(target.gameObject.tag == Tags.PLATFORM_TAG)
+        {
+            if(playerJumped)
+            {
+                playerJumped = false;
+                //playerAnim.Didland();
+            }
+        }
+    }
+
+
+
+
+
+} // Player Class
