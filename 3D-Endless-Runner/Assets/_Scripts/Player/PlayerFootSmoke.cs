28e44b860a2b1175daa6ae8f882c27d3952ad87b
Jose <joey_manuel@outlook.com>
2020-12-27 12:08:37 -0500

    6.Player Walk Sound
    Created the player walk sound effect.
    PlayerSounds.cs file created

---
+++ 3D-Endless-Runner/Assets/_Scripts/Player/PlayerFootSmoke.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFootSmoke : MonoBehaviour
+{
+    #region Variables
+    public GameObject smokeEffect;
+    public GameObject smokePosition;
+    #endregion
+
+
+    private void OnTriggerEnter(Collider target)
+    {
+        if(target.tag == Tags.PLATFORM_TAG)
+        {
+            if(smokePosition.activeInHierarchy)
+            {
+                Instantiate(smokeEffect, smokePosition.transform.position, Quaternion.identity);
+            }
+        }
+    }
+} // PlayerFootSmoke class
---
+++ 3D-Endless-Runner/Assets/_Scripts/Player/PlayerSounds.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSounds : MonoBehaviour
+{
+    #region Variables
+    private float collissionSoundEffect = 1f;
+
+    public float audioFootVolume = 1f;
+    public float soundEffectPitchRandomness = 0.05f;
+
+    private AudioSource audioSource;
+    public AudioClip genericFoodSound;
+    public AudioClip metalFootSound;
+
+    #endregion
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+    }
+
+    void FootSound()
+    {
+        audioSource.volume = collissionSoundEffect * audioFootVolume;
+        audioSource.pitch = Random.Range(1.0f - soundEffectPitchRandomness, 1.0f + soundEffectPitchRandomness);
+
+        if(Random.Range(0,2) > 0)
+        {
+            audioSource.clip = genericFoodSound;
+        }
+        else
+        {
+            audioSource.clip = metalFootSound;
+        }
+    }
+
+
+
+
+} // PlayerSounds class
