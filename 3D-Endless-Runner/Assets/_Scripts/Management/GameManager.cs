18b107d30337b11e209dab1210d62ded6dec1d61
Jose <joey_manuel@outlook.com>
2021-01-02 09:55:31 -0500

    10.MainMenu Scene, GameManager, GameplayController
    created the latest files, MainMenu scene, GamePLay Controller and GameManager.
    Created a new scene.
    
    Project is ready for merging.

---
+++ 3D-Endless-Runner/Assets/_Scripts/Management/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region Variables
+    public static GameManager instance;
+
+    public bool gameStartedFromMainMenu, gameRestartedPlayerDied;
+
+    [HideInInspector]
+    public float score, health, level;
+
+    [HideInInspector]
+    public bool canPlayMusic = true;
+
+    #endregion
+
+    private void Awake()
+    {
+        MakeSingleton();
+    }
+
+    private void MakeSingleton()
+    {
+        if(instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+    }
+
+
+} // GameManager class
---
+++ 3D-Endless-Runner/Assets/_Scripts/Management/MainMenuController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    #region Varaibles
+    [SerializeField]
+    private Button musicBtn;
+
+    [SerializeField]
+    private Sprite soundOff, SoundOn;
+    #endregion
+
+    public void PlayGame()
+    {
+        GameManager.instance.gameStartedFromMainMenu = true;
+        SceneManager.LoadScene(Tags.GAMEPLAY_SCENE);
+
+    }
+
+    public void ControlMusic()
+    {
+        if(GameManager.instance.canPlayMusic)
+        {
+            musicBtn.image.sprite = SoundOn;
+            GameManager.instance.canPlayMusic = false;
+        }
+        else
+        {
+            musicBtn.image.sprite = soundOff;
+            GameManager.instance.canPlayMusic = true;
+        }
+    }
+
+
+
+
+
+} // MainMenuController class
